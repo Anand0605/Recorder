20574f6cc9f8248160adb07e73c73d6aa5f394a0
Anand0605 <anandvns0510@gmail.com>
2023-07-19 13:36:16 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Univarsal Media Recorder
+A media recorder with the features like screen recording , video
+recording,audio recording . and user can download what ever the
+recorded using universal media recorder
+
+## Tech-Stack
+ React and react media recorder
+ ## Home page
+ ![image](https://github.com/ashutosh2720/universal-recorder/assets/109720375/00b601cc-42ac-4b07-b890-dd231c42e2cb)
+ ## 
+![image](https://github.com/ashutosh2720/universal-recorder/assets/109720375/ac8bf676-344c-4af2-a6fc-7611687612ad)
+##
+![image](https://github.com/ashutosh2720/universal-recorder/assets/109720375/7489cb65-57ec-49f2-997b-ad2b2221ede6)
+
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import '../css/Nav.css'
+import Github from '../images/github.png'
+import { AiFillGithub, } from 'react-icons/ai'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaTwitterSquare, FaGithubSquare } from 'react-icons/fa'
+export default function NavBar() {
+    return <>
+        <div className="nav">
+            <div className="nav-left">
+                <h3>𝓡𝓔𝓒𝓞𝓡𝓓𝓔𝓡</h3>
+            </div>
+            <div className="nav-right">
+                <div className="social">
+                    <ul>
+                        <li><FaGithubSquare className='github' /></li>
+                        <li><BsLinkedin className='linkdin' /></li>
+                        <li><FaTwitterSquare className='twiter' /></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </>
+}
